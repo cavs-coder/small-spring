b90ba03bd0098ef8fa784f20f9147a2329aba185
fuguangwei <fuguangwei@xg.com>
2023-07-12 08:50:23 +0800

    small-spring-step-01 v1

---
+++ small-spring-step-01/src/test/java/com/study/springframework/SmallSpringStep01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSpringStep01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ small-spring-step-02/src/test/java/com/study/springframework/SmallSpringStep02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSpringStep02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
