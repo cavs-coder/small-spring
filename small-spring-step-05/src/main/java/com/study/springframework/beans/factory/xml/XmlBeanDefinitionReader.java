364ed8e636daf0eaa5e34af06ff44e77859c7a97
fuguangwei <fuguangwei@xg.com>
2023-07-30 23:46:30 +0800

    small-spring-step-05 v2

--- small-spring-step-05/src/main/java/com/study/springframework/beans/factory/xml/XmlBeanDefinitionReader.java
+++ small-spring-step-05/src/main/java/com/study/springframework/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -1,10 +1,19 @@
 package com.study.springframework.beans.factory.xml;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.XmlUtil;
 import com.study.springframework.beans.BeansException;
+import com.study.springframework.beans.PropertyValue;
+import com.study.springframework.beans.factory.config.BeanDefinition;
+import com.study.springframework.beans.factory.config.BeanReference;
 import com.study.springframework.beans.factory.support.AbstractBeanDefinitionReader;
 import com.study.springframework.beans.factory.support.BeanDefinitionRegistry;
 import com.study.springframework.core.io.Resource;
 import com.study.springframework.core.io.ResourceLoader;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,7 +36,7 @@ public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
     public void loadBeanDefinitions(Resource resource) throws BeansException {
         try (InputStream inputStream = resource.getInputStream()) {
             doLoadBeanDefinitions(inputStream);
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             throw new BeansException("IOException parsing XML document from " + resource, e);
         }
     }
@@ -46,8 +55,65 @@ public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
         loadBeanDefinitions(resource);
     }
 
-    private void doLoadBeanDefinitions(InputStream inputStream) {
+    private void doLoadBeanDefinitions(InputStream inputStream) throws ClassNotFoundException {
+        Document doc = XmlUtil.readXML(inputStream);
+        Element root = doc.getDocumentElement();
+        NodeList childNodes = root.getChildNodes();
 
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node item = childNodes.item(i);
+            //判断元素
+            if (!(item instanceof Element)) {
+                continue;
+            }
+            //判断对象
+            if (!"bean".equals(item.getNodeName())) {
+                continue;
+            }
+
+            //解析标签
+            Element bean = (Element) item;
+            String id = bean.getAttribute("id");
+            String name = bean.getAttribute("name");
+            String className = bean.getAttribute("class");
+
+            //获取class，方便获取类中的名称
+            Class<?> clazz = Class.forName(className);
+            //优先级 id > name
+            String beanName = StrUtil.isNotEmpty(id) ? id : name;
+            if (StrUtil.isEmpty(beanName)) {
+                beanName = StrUtil.lowerFirst(clazz.getSimpleName());
+            }
+
+            //bean定义
+            BeanDefinition beanDefinition = new BeanDefinition(clazz);
+            //读取属性并填充
+            for (int j = 0; j < bean.getChildNodes().getLength(); j++) {
+                Node beanItem = bean.getChildNodes().item(j);
+                //判断元素
+                if (!(beanItem instanceof Element)) {
+                    continue;
+                }
+                //判断对象
+                if (!"property".equals(beanItem.getNodeName())) {
+                    continue;
+                }
+                Element property = (Element) beanItem;
+                String attrName = property.getAttribute("name");
+                String attrValue = property.getAttribute("value");
+                String attrRef = property.getAttribute("ref");
+                //获取属性值：引入对象、值对象
+                Object value = StrUtil.isNotEmpty(attrRef) ? new BeanReference(attrRef) : attrValue;
+                //创建属性信息
+                PropertyValue propertyValue = new PropertyValue(attrName, value);
+                beanDefinition.getPropertyValues().addPropertyValue(propertyValue);
+            }
+            if (getRegistry().containsBeanDefinition(beanName)) {
+                throw new BeansException("Duplicate beanName[" + beanName + "] is not allowed");
+            }
+            //注册beanDefinition
+            getRegistry().registerBeanDefinition(beanName, beanDefinition);
+        }
     }
 
 }
---
+++ small-spring-step-05/src/test/java/com/study/springframework/ApiTestResourceLoader.java
@@ -0,0 +1,45 @@
+package com.study.springframework;
+
+import cn.hutool.core.io.IoUtil;
+import com.study.springframework.core.io.DefaultResourceLoader;
+import com.study.springframework.core.io.Resource;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author fuguangwei
+ * @date 2023-07-30
+ */
+public class ApiTestResourceLoader {
+
+    private DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+
+    @Test
+    public void test_classpath() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_file() throws IOException {
+        Resource resource = resourceLoader.getResource("src/test/resources/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_url() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+
+
+}
